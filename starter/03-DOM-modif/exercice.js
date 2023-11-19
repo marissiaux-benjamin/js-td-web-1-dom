ca7d9f0d115fd3d277268f7f380530f6413095cf
benjamin <benjamin.marissiaux@student.hepl.be>
2023-11-19 16:31:42 +0100

    cours.js

--- starter/03-DOM-modif/exercice.js
+++ starter/03-DOM-modif/exercice.js
@@ -18,3 +18,37 @@ et un titre "Mes jeux". Écrire le code JS qui
   d'id "jeux" créée dynamiquement qui reprend les jeux suivis de
   leur catégorie entre parenthèses  
   */
+
+const liste = document.createElement('ul');
+liste.setAttribute('id', 'jeux');
+document.body.appendChild(liste);
+console.log(document.getElementById('jeu'));
+
+const lol = document.createElement('li');
+lol.textContent = 'League of Legends : jeu de stratégie/action';
+lol.setAttribute('id', 'League of Legends');
+liste.appendChild(lol);
+
+const wow = document.createElement('li');
+wow.textContent = 'World of Warcraft : MMORPG (jeu de rôle en ligne massivement multijoueur)';
+wow.setAttribute('id', 'World of Warcraft');
+liste.appendChild(wow);
+
+const cod = document.createElement('li');
+cod.textContent = 'Call of Duty : FPS (jeu de tir)';
+cod.setAttribute('id', 'Call of Duty');
+liste.appendChild(cod);
+
+const fifa = document.createElement('li');
+fifa.textContent = 'Fifa 2020 : jeu de simulation';
+fifa.setAttribute('id', 'Fifa2020');
+liste.appendChild(fifa);
+
+console.log(document.getElementById('jeux').children);
+
+
+const tableau = [document.getElementById('jeux').children];
+
+
+
+
