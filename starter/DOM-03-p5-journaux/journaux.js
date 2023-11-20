090c2b582cb31a2294c9cd6f1b770e3dde581faf
benjamin <benjamin.marissiaux@student.hepl.be>
2023-11-20 20:33:30 +0100

    journaux.js

--- starter/DOM-03-p5-journaux/journaux.js
+++ starter/DOM-03-p5-journaux/journaux.js
@@ -15,3 +15,32 @@ Il a sans doute été simplifié à outrance dans un souci
 de rendre la génération la plus simple possible en JS.
 TODO : améliorer le balisage HTML généré !!! */
 
+const url1 = 'http://lemonde.fr';
+const url2 = 'http://lefigaro.fr';
+const url3 = 'http://liberation.fr';
+const urlJournaux = [url1, url2,url3];
+console.log(urlJournaux);
+
+
+let laListe = document.createElement('ul');
+laListe.setAttribute('id', 'l');
+document.getElementById('contenu').appendChild(laListe);
+
+
+
+let lesLiens = document.createElement('li');
+lesLiens.setAttribute('id', 'journal1');
+lesLiens.innerHTML = 'Le <a href="http://lemonde.fr">Monde<a/>';
+document.getElementById('l').appendChild(lesLiens);
+
+let lesLiens2 = document.createElement('li');
+lesLiens2.setAttribute('id', 'journal2');
+lesLiens2.innerHTML = 'Le <a href="http://lefigaro.fr">Figaro<a/>'
+document.getElementById('l').appendChild(lesLiens2);
+
+let lesLiens3 = document.createElement('li');
+lesLiens3.setAttribute('id', 'journal3');
+lesLiens3.innerHTML = '<a href="http://liberation.fr">Libération<a/>'
+document.getElementById('l').appendChild(lesLiens3);
+
+
